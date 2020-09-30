1db887f8f221101257dd6681dc541fc7d43d767a
hinsenoo <837545254@qq.com>
2020-10-01 02:19:00 +0800

    koa-router 基本使用、多中间件、前缀功能

--- node相关.md
+++ node相关.md
@@ -126,3 +126,111 @@ app.listen(3000);
 - 处理不同的 URL
 - 处理不同的 HTTP 方法
 - 解析 URL 上的参数
+
+## 编写 Koa 路由中间件
+
+### 操作步骤
+
+- 处理不同的 URL
+- 处理不同的 HTTP 方法
+- 解析 URL 上的参数
+
+```js
+const Koa = require('koa');
+const app = new Koa();
+
+// use 使用中间件
+app.use(async (ctx)=>{
+    if(ctx.url === '/'){
+        ctx.body = '这是主页';
+    }else if(ctx.url === '/users'){				// 处理不同的 URL
+        if(ctx.method === 'GET'){				// 处理不同的 HTTP 方法
+            ctx.body = '这是用户列表页';
+        }else if(ctx.method === 'POST'){
+            ctx.body = '创建用户';
+        }else{
+            // 405：不允许这个方法
+            ctx.status = 405;
+        }
+    }else if(ctx.url.match(/\/users\/\w+/)){	// 解析 URL 上的参数
+        // 匹配正则：\w 匹配字母 + 一个或多个
+        // 匹配符合小括号内的内容: userId
+        // [
+        //     "/users/111",
+        //     "111"    
+        // ]
+        const userId = ctx.url.match(/\/users\/(\w+)/)[1];
+        ctx.body = `这是用户 ${userId}`;
+    }else {
+        // 404：服务器上没有找到请求的资源
+        ctx.status = 404;
+    }
+});
+
+app.listen(3000);
+```
+
+## 使用 koa-router 实现路由
+
+- 更优雅的实现路由基本功能
+- 演示一些高级路由功能，如前缀、多中间件
+
+```js
+const Koa = require('koa');
+const Router = require('koa-router');
+const app = new Koa();
+const router = new Router();
+// 前缀替代
+const usersRouter = new Router({ prefix: '/users' });
+
+// 多中间件，使用场景：用户校验、数据安全（分层系统里面的安全层);
+// 鉴权
+const auth = async(ctx, next) => {
+    if(ctx.url !== 'users'){
+        ctx.throw(401);
+    }
+};
+
+// 处理不同的 URL
+router.get('/', (ctx)=>{
+    ctx.body = '这是主页';
+});
+// 正常写法=============================
+// router.get('/users', (ctx)=>{
+//     ctx.body = '这是用户列表';
+// });
+
+// // 处理不同的 HTTP 方法
+// router.post('/users', (ctx)=>{
+//     ctx.body = '创建用户';
+// });
+
+// // 解析 URL 参数
+// router.get('/users/:id', (ctx)=>{
+//     ctx.body = `这是用户 ${ctx.params.id}`;
+// });
+
+// 前缀写法============================= 
+usersRouter.get('/', (ctx)=>{
+    ctx.body = '这是用户列表';
+});
+usersRouter.post('/', (ctx)=>{
+    ctx.body = '创建用户';
+});
+// 多中间件，仿鉴权
+usersRouter.get('/:id',auth, (ctx)=>{
+    ctx.body = `这是用户 ${ctx.params.id}`;
+});
+
+// router.routes() 来加载路由中间件
+app.use(router.routes());
+// 前缀路由中间件
+app.use(usersRouter.routes());
+
+
+app.listen(3000);
+
+
+
+```
+
--- project/index.js
+++ project/index.js
@@ -1,34 +1,83 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 const app = new Koa();
+const router = new Router();
+// 前缀替代
+const usersRouter = new Router({ prefix: '/users' });
+
+// 多中间件，使用场景：用户校验、数据安全（分层系统里面的安全层);
+// 鉴权
+const auth = async(ctx, next) => {
+    if(ctx.url !== 'users'){
+        ctx.throw(401);
+    }
+};
+
+// 处理不同的 URL
+router.get('/', (ctx)=>{
+    ctx.body = '这是主页';
+});
+// 正常写法=============================
+// router.get('/users', (ctx)=>{
+//     ctx.body = '这是用户列表';
+// });
+
+// // 处理不同的 HTTP 方法
+// router.post('/users', (ctx)=>{
+//     ctx.body = '创建用户';
+// });
+
+// // 解析 URL 参数
+// router.get('/users/:id', (ctx)=>{
+//     ctx.body = `这是用户 ${ctx.params.id}`;
+// });
+
+// 前缀写法============================= 
+usersRouter.get('/', (ctx)=>{
+    ctx.body = '这是用户列表';
+});
+usersRouter.post('/', (ctx)=>{
+    ctx.body = '创建用户';
+});
+usersRouter.get('/:id',auth, (ctx)=>{
+    ctx.body = `这是用户 ${ctx.params.id}`;
+});
+
+// router.routes() 来加载路由中间件
+app.use(router.routes());
+app.use(usersRouter.routes());
+
+
 
 // use 使用中间件
-app.use(async (ctx)=>{
-    if(ctx.url === '/'){
-        ctx.body = '这是主页';
-    }else if(ctx.url === '/users'){
-        if(ctx.method === 'GET'){
-            ctx.body = '这是用户列表页';
-        }else if(ctx.method === 'POST'){
-            ctx.body = '创建用户';
-        }else{
-            // 405：不允许这个方法
-            ctx.status = 405;
-        }
-    }else if(ctx.url.match(/\/users\/\w+/)){
-        // 匹配正则：\w 匹配字母 + 一个或多个
-        // 匹配符合小括号内的内容: userId
-        // [
-        //     "/users/111",
-        //     "111"    
-        // ]
-        const userId = ctx.url.match(/\/users\/(\w+)/)[1];
-        ctx.body = `这是用户 ${userId}`;
+// 编写 koa 路由中间件
+// app.use(async (ctx)=>{
+//     if(ctx.url === '/'){
+//         ctx.body = '这是主页';
+//     }else if(ctx.url === '/users'){
+//         if(ctx.method === 'GET'){
+//             ctx.body = '这是用户列表页';
+//         }else if(ctx.method === 'POST'){
+//             ctx.body = '创建用户';
+//         }else{
+//             // 405：不允许这个方法
+//             ctx.status = 405;
+//         }
+//     }else if(ctx.url.match(/\/users\/\w+/)){
+//         // 匹配正则：\w 匹配字母 + 一个或多个
+//         // 匹配符合小括号内的内容: userId
+//         // [
+//         //     "/users/111",
+//         //     "111"    
+//         // ]
+//         const userId = ctx.url.match(/\/users\/(\w+)/)[1];
+//         ctx.body = `这是用户 ${userId}`;
 
-    }else {
-        // 404：服务器上没有找到请求的资源
-        ctx.status = 404;
-    }
-});
+//     }else {
+//         // 404：服务器上没有找到请求的资源
+//         ctx.status = 404;
+//     }
+// });
 
 app.listen(3000);
 
